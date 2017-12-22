662b4b6b2a760b75f7b2cbea5695cbb8b24d6ad0
Christian Kraus <Christian.Kraus@sap.com>
2017-12-22 20:17:16 +0100

    Initial 2

---
+++ src/main/java/jupiterpi/readingproject/controller/ProjectController.java
@@ -0,0 +1,21 @@
+package jupiterpi.readingproject.controller;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(path = "/project")
+@RestController
+public class ProjectController {
+	@GetMapping("")
+	public String getHello() {
+		return "Hello World";
+	}
+}
